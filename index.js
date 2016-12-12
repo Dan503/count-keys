1697fb630ac8198ede4443e4635916d0f722cf38
Daniel Tonon <daniel.tonon@adelphi.digital>
2016-12-12 19:35:27 +1100

    Bebelified src.js

--- index.js
+++ index.js
@@ -1,38 +1,52 @@
+'use strict';
 
-import defaultTo from 'default-to';
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
 
-export default function countKeys (obj, spec){
-	spec = defaultTo(spec, {
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+exports.default = countKeys;
+
+var _defaultTo = require('default-to');
+
+var _defaultTo2 = _interopRequireDefault(_defaultTo);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function countKeys(obj, spec) {
+	spec = (0, _defaultTo2.default)(spec, {
 		recursive: true,
 		arrays: true,
-		filter: (item) => true,
-		maxDepth: 'none',
+		filter: function filter(item) {
+			return true;
+		},
+		maxDepth: 'none'
 	});
 
-	let count = 0;
+	var count = 0;
 
-	function raiseCount(thisObject, parent, depth){
-		let returnVal = count;
+	function raiseCount(thisObject, parent, depth) {
+		var returnVal = count;
 
+		thisObject = (0, _defaultTo2.default)(thisObject, {});
 
-		thisObject = defaultTo(thisObject, {});
+		for (var key in thisObject) {
 
-		for (const key in thisObject){
+			if (thisObject.hasOwnProperty(key)) {
+				var value = thisObject[key];
 
-			if (thisObject.hasOwnProperty(key)){
-				const value = thisObject[key];
+				var arrayCheck = spec.arrays ? true : isNaN(parseInt(key));
 
-				const arrayCheck = spec.arrays ? true : isNaN(parseInt(key));
+				var filter = spec.filter.call(thisObject, { key: key, value: value, count: count, parent: parent, depth: depth });
+				filter = (0, _defaultTo2.default)(filter, true);
 
-				let filter = spec.filter.call(thisObject, { key, value, count, parent, depth });
-				filter = defaultTo(filter, true);
+				if (filter && arrayCheck) count++;
 
-				if (filter && arrayCheck) count ++;
+				var isUnderMaxDepth = isNaN(spec.maxDepth) ? true : depth < spec.maxDepth;
+				var isRecursive = spec.recursive && (typeof value === 'undefined' ? 'undefined' : _typeof(value)) === 'object';
 
-				const isUnderMaxDepth = isNaN(spec.maxDepth) ? true : depth < spec.maxDepth;
-				const isRecursive = spec.recursive && typeof value === 'object';
-
-				if (isUnderMaxDepth && isRecursive){
+				if (isUnderMaxDepth && isRecursive) {
 					returnVal = raiseCount(value, thisObject, depth + 1);
 				} else {
 					returnVal = count;
@@ -44,3 +58,4 @@ export default function countKeys (obj, spec){
 	}
 	return raiseCount(obj, false, 1);
 }
+module.exports = exports['default'];
